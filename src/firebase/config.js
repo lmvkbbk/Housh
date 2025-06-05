a2db9a7503333005ea6c427f6ed1fc234f375327
Liam Vitor <166427246+lmvkbbk@users.noreply.github.com>
2025-06-05 00:22:45 -0300

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -9,14 +9,7 @@ import { getDatabase } from "firebase/database";
 import { Platform } from "react-native";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCZU6myY_MkuvjcWQaq-kZ-ID84ZZZmmYU",
-    authDomain: "goal-rush-1947d.firebaseapp.com",
-    databaseURL: "https://goal-rush-1947d-default-rtdb.firebaseio.com",
-    projectId: "goal-rush-1947d",
-    storageBucket: "goal-rush-1947d.firebasestorage.app",
-    messagingSenderId: "592969987743",
-    appId: "1:592969987743:web:9024768744ace609d414aa",
-    measurementId: "G-J1CQ7Q4VW7",
+    //Chaves de api do firebase
 };
 
 const app = initializeApp(firebaseConfig);
